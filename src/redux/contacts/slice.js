de8ac2559d3ab76e9f68b5a533c64fedfedb9f97
Antonina Grebeniuk <rebel.t2808@gmail.com>
2024-06-16 21:53:54 +0400

    add filter by number

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -47,19 +47,4 @@ const slice = createSlice({
   },
 });
 
-// export const selectContacts = state => state.contacts.items;
-
-// export const selectLoading = state => state.contacts.loading;
-
-// export const selectError = state => state.contacts.error;
-
-// export const selectVisibleContacts = createSelector(
-//   [selectContacts, selectNameFilter],
-//   (contacts, filter) => {
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(filter.toLowerCase()),
-//     );
-//   },
-// );
-
 export default slice.reducer;
